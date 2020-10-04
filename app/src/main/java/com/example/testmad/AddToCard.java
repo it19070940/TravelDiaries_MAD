4cbd9ddfc824f35d1db4fefa66e18146aff6bd95
KthusshandR <kithusshandraveendran@gmail.com>
2020-10-04 13:30:23 +0530

    AddToCard

---
+++ app/src/main/java/com/example/testmad/AddToCard.java
@@ -0,0 +1,14 @@
+package com.example.testmad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddToCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_card);
+    }
+}
